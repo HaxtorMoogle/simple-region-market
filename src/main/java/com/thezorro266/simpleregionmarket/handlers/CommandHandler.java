0c14c366f6ee8d4b0361a963c5bde49ee6c8d84e
Benedikt Ziemons <B.Ziemons@gmx.de>
2012-05-23 20:15:07 +0200

    Corrected untake command + language support

--- src/main/java/com/thezorro266/simpleregionmarket/handlers/CommandHandler.java
+++ src/main/java/com/thezorro266/simpleregionmarket/handlers/CommandHandler.java
@@ -1,8 +1,7 @@
 package com.thezorro266.simpleregionmarket.handlers;
 
+import java.util.ArrayList;
 import java.util.logging.Level;
-
-import javax.annotation.Untainted;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -10,8 +9,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import com.sk89q.worldguard.domains.DefaultDomain;
-import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.thezorro266.simpleregionmarket.SimpleRegionMarket;
 import com.thezorro266.simpleregionmarket.TokenManager;
 import com.thezorro266.simpleregionmarket.Utils;
@@ -37,8 +34,10 @@ public class CommandHandler implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
 		Player player = null;
+		Boolean isConsole = true;
 		if (sender instanceof Player) {
 			player = (Player) sender;
+			isConsole = false;
 		}
 
 		if (args.length < 1) {
@@ -52,77 +51,56 @@ public class CommandHandler implements CommandExecutor {
 				langHandler.consoleDirectOut(Level.INFO, "loaded version " + plugin.getDescription().getVersion() + ",  " + Utils.getCopyright());
 			}
 		} else if (args[0].equalsIgnoreCase("untake")) {
-			Boolean IsConsole = false;
-			if (player == null)
-			{
-				IsConsole = true;
-			}
-			if (args.length < 2) {
-				if (IsConsole) {
-				} else {
-					langHandler.playerListOut(player, "CMD.UNTAKE.NO_ARG", null);
-					return true;
-				}
-				if(args[1].isEmpty())
-				{
-					langHandler.playerDirectOut(player, ChatColor.DARK_RED, "You haven't specified region");
-					return true;
-				}
-				String region = args[1];
-				String world = "";
-				if(args[2].isEmpty())
-				{
-					if (IsConsole) {
-						langHandler.consoleDirectOut(Level.INFO, "You must specify world");
-						return true;
+			if (isConsole || SimpleRegionMarket.permManager.isAdmin(player)) {
+				if (args.length < 2) {
+					if (isConsole) {
+						langHandler.consoleOut("CMD.UNTAKE.NO_ARG", Level.INFO, null);
+					} else {
+						langHandler.playerListOut(player, "CMD.UNTAKE.NO_ARG", null);
 					}
-					world = player.getWorld().getName();
-				}
-				else
-				{
-					world = args[2];
-				}
-				if(!SimpleRegionMarket.permManager.isAdmin(player))
-				{
-					langHandler.playerDirectOut(player, ChatColor.DARK_RED, "You do not have permission to do that");
 					return true;
-				}
-				Boolean found = false;
-				for(final TemplateMain token : TokenManager.tokenList)
-				{
-					if(Utils.getEntry(token, world, region, "taken") != null)
-					{
-						if(Utils.getEntryBoolean(token, world, region, "taken"))
-						{
-							found = true;
-							token.untakeRegion(world, region);
+				} else {
+					final String region = args[1];
+					String world;
+					if (args.length > 2) {
+						world = args[2];
+					} else {
+						if (isConsole) {
+							langHandler.consoleOut("CMD.UNTAKE.CONSOLE_NOWORLD", Level.SEVERE, null);
+							return true;
+						} else {
+							world = player.getWorld().getName();
+						}
+					}
+					Boolean found = false;
+					for (final TemplateMain token : TokenManager.tokenList) {
+						if (Utils.getEntry(token, world, region, "taken") != null) {
+							if (Utils.getEntryBoolean(token, world, region, "taken")) {
+								token.untakeRegion(world, region);
+								found = true;
+								break;
+							}
+						}
+					}
+					final ArrayList<String> list = new ArrayList<String>();
+					list.add(region);
+					list.add(world);
+					if (found) {
+						if (isConsole) {
+							langHandler.consoleOut("CMD.UNTAKE.SUCCESS", Level.INFO, list);
+						} else {
+							langHandler.playerNormalOut(player, "CMD.UNTAKE.SUCCESS", list);
+						}
+					} else {
+						if (isConsole) {
+							langHandler.consoleOut("CMD.UNTAKE.NO_REGION", Level.WARNING, list);
+						} else {
+							langHandler.playerErrorOut(player, "CMD.UNTAKE.NO_REGION", list);
 						}
 					}
 				}
-				if(found)
-				{
-					if(IsConsole)
-					{
-						langHandler.consoleDirectOut(Level.INFO, "Region " + region + " has been successfully untaken");
-					}
-					else
-					{
-						langHandler.playerDirectOut(player, ChatColor.YELLOW, "Region " + region + " has been successfully untaken");
-					}
-					return true;
-				}
-				else
-				{
-					if(IsConsole)
-					{
-						langHandler.consoleDirectOut(Level.INFO, "Region hasn't been found in this world");
-					}
-					else
-					{
-						langHandler.playerDirectOut(player, ChatColor.DARK_RED, "Region hasn't been found in this world");
-					}
-					return true;
-				}
+			} else {
+				langHandler.playerErrorOut(player, "PLAYER.NO_PERMISSIONS.NORM", null);
 			}
 		} else if (args[0].equalsIgnoreCase("remove")) {
 			if (args.length < 2) {
@@ -131,9 +109,7 @@ public class CommandHandler implements CommandExecutor {
 				} else {
 					langHandler.playerListOut(player, "CMD.REMOVE.NO_ARG", null);
 				}
-				return true;
 			}
-
 		} else if (args[0].equalsIgnoreCase("list")) { // TODO Can list own and rented regions
 			if (player == null) {
 				langHandler.consoleDirectOut(Level.INFO, "Not yet implemented");
@@ -152,7 +128,6 @@ public class CommandHandler implements CommandExecutor {
 			} else {
 				langHandler.playerDirectOut(player, ChatColor.BLUE, "Not yet implemented");
 			}
-			final ProtectedRegion protectedRegion = SimpleRegionMarket.wgManager.getProtectedRegion(Bukkit.getWorld(world), region);
 		} else if (args[0].equalsIgnoreCase("remmember") || args[0].equalsIgnoreCase("removemember")) {
 			if (player == null) {
 				langHandler.consoleDirectOut(Level.INFO, "Not yet implemented");
